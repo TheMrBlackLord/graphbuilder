b76fc06b1ab413562abb3f4d7a599d0244bc3543
MrBlackLord <theblacklord05@gmail.com>
2020-12-18 10:54:58 +0300

    update

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+   <header class="header">
+      <div class="container">
+         <div class="row">
+            <div class="nav neon-border1">
+               <div class="col">
+                  <h1 class="logo">Graph builder</h1>
+               </div>
+            </div> 
+         </div>
+      </div>
+   </header>
+</template>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,32 @@
+<template>
+   <aside class="toolbar">
+      <div class="toolbar-header">
+         <p class="title">Tools</p>
+         <button class="btn btn-aside-close aside-close-js" type="button">
+            <i class="fas fa-angle-double-left"></i>
+         </button>
+      </div>
+      <div class="toolbar-box">
+         <ul class="toollist">
+            <li class="tool">
+               <button class="btn btn-tool" type="button">
+                  <i class="tool-icon fas fa-search"></i>
+                  <p class="tool-name">Search</p>
+               </button>
+            </li>
+            <li class="tool">
+               <button class="btn btn-tool" type="button">
+                  <i class="tool-icon fas fa-search"></i>
+                  <p class="tool-name">Search</p>
+               </button>
+            </li>
+            <li class="tool">
+               <button class="btn btn-tool" type="button">
+                  <i class="tool-icon fas fa-search"></i>
+                  <p class="tool-name">Search</p>
+               </button>
+            </li>
+         </ul>
+      </div>
+   </aside>
+</template>
